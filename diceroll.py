d8c681767e38f3cb76c18c5e54aeb589b05fa6db
Timm Murray <tmurray@wumpus-cave.net>
2018-09-29 15:29:45 -0500

    Throw exceptions for dice rolls beyond limits

--- diceroll.py
+++ diceroll.py
@@ -1,6 +1,25 @@
 import random
 
+class DiceTooBigException(Exception):
+    def __init__( self, asked_size, max_allowed_size ):
+        self.asked_size = asked_size
+        self.max_allowed_size = max_allowed_size
+
+class TooManyDiceException(Exception):
+    def __init__( self, asked_dice, max_allowed_dice ):
+        self.asked_dice = asked_dice
+        self.max_allowed_size = max_allowed_dice
+
 class DiceRoll:
-    def roll_dice( size_dice, num_dice=1 ):
+    def __init__( self, max_dice=128, max_size=1024 ):
+        self.max_dice = max_dice
+        self.max_size = max_size
+
+    def roll( self, size_dice, num_dice=1 ):
+        if size_dice > self.max_size:
+            raise DiceTooBigException( size_dice, self.max_size )
+        if num_dice > self.max_dice:
+            raise TooManyDiceException( num_dice, self.max_dice )
+
         rolls = map( lambda x: random.randint( 1, size_dice ), range(num_dice) )
         return list(rolls)
--- roll_test.py
+++ roll_test.py
@@ -4,11 +4,19 @@ import diceroll
 class TestRoll(unittest.TestCase):
     '''roll dice'''
     def test_roll(self):
-        roll = diceroll.DiceRoll.roll_dice( 6 )
+        roll = diceroll.DiceRoll().roll( 6 )
         self.assertEqual( len(roll), 1, "Correct number of rolls" )
         self.assertLessEqual( roll[0], 6, "Correct top size" )
         self.assertGreaterEqual( roll[0], 1, "Correct bottom size" );
 
     def test_many_rolls(self):
-        roll = diceroll.DiceRoll.roll_dice( 10, 5 )
+        roll = diceroll.DiceRoll().roll( 10, 5 )
         self.assertEqual( len(roll), 5, "Correct number of rolls" )
+
+    def test_limits(self):
+        dice = diceroll.DiceRoll( max_dice=5, max_size=20 )
+        dice.roll( 20, 5 )
+        self.assertTrue( True, "Can roll dice at limit" )
+        
+        self.assertRaises( diceroll.DiceTooBigException, dice.roll, 21, 5 )
+        self.assertRaises( diceroll.TooManyDiceException, dice.roll, 20, 6 )
