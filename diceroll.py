70aa285882fa6ba1bddcc174d01907d626fa6d58
tmurray <tmurray@wumpus-cave.net>
2018-10-01 18:13:52 -0500

    Documentation

--- diceroll.py
+++ diceroll.py
@@ -7,11 +7,13 @@ roll_match = re.compile( "(\d+)?d(\d+)" )
 
 
 class DiceTooBigException(Exception):
+    """Thrown when the size of the die is too big"""
     def __init__( self, asked_size, max_allowed_size ):
         self.asked_size = asked_size
         self.max_allowed_size = max_allowed_size
 
 class TooManyDiceException(Exception):
+    """Thrown when the number of dice is too big"""
     def __init__( self, asked_dice, max_allowed_dice ):
         self.asked_dice = asked_dice
         self.max_allowed_dice = max_allowed_dice
@@ -22,6 +24,7 @@ class DiceRoll:
         self.max_size = max_size
 
     def roll( self, size_dice, num_dice=1 ):
+        """Rolls dice of the given size and number. Returns a list of the rolls."""
         if size_dice > self.max_size:
             raise DiceTooBigException( size_dice, self.max_size )
         if num_dice > self.max_dice:
@@ -31,6 +34,7 @@ class DiceRoll:
         return list(rolls)
 
 class DiceRollHandler( mentionrouter.Handler ):
+    """A mentionrouter.Handler that parses the message and rolls dice accordingly"""
     def __init__( self, max_dice, max_dice_size, slack_client ):
         self.max_dice = max_dice
         self.max_dice_size = max_dice_size
--- mentionrouter.py
+++ mentionrouter.py
@@ -9,27 +9,34 @@ trim_trailing_whitespace = re.compile( '\s*$' )
 
 
 class Handler:
+    """Handles commands sent by the Router"""
     @abstractmethod
     def handle_mention( self, from_user, to_user, cmd, remaining_msg, channel ):
+        """Called when the Router decides you should be the one to handle a message"""
         pass
 
 class DefaultLastResortRouter( Handler ):
+    """The Handler that gets hit by default when nothing else does"""
     def handle_mention( self, from_user, to_user, cmd, remaining_msg, channel ):
         # TODO message failure back to the user
         pass
 
 class Router:
+    """Takes registration of Handler objects, and routes to them when the command is hit"""
     def __init__( self ):
         self.last_resort_handler = DefaultLastResortRouter()
         self.cmds = {}
 
     def set_last_resort_handler( handler ):
+        """Set the Handler that gets hit when nothing else matches"""
         self.last_resort_handler = handler
 
     def register( self, cmd, handler ):
+        """Set a Handler to be hit when the given command string is made"""
         self.cmds[cmd] = handler
 
     def handle_mention( self, user, text, channel ):
+        """When the bot user is mentioned, this gets called with the text of the chat"""
         to_user, cmd, remaining_msg = self._parse_msg( text )
         handler = self.cmds[cmd] if cmd in self.cmds else self.last_resort_handler
         handler.handle_mention( user, to_user, cmd, remaining_msg, channel )
